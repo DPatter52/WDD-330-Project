7531d9a53982324fdce26a3e2a799c55b39252db
DPatter52 <dzpatterson15@gmail.com>
2022-09-28 17:16:41 -0600

    format

--- src/js/utils.js
+++ src/js/utils.js
@@ -38,25 +38,24 @@ export function renderListWithTemplate(template,parentElement,list,callback) {
   });
 }
 
-export function renderWithTemplate(template,parentElement,data,callback) {
-    let clone = template.contentEditable.cloneNode(true);
-    if(callback)
-      clone = callback(clone, data);
-    parentElement.appendChild(clone);
+export function renderWithTemplate(template, parentElement, data, callback) {
+  let clone = template.contentEditable.cloneNode(true);
+  if (callback) clone = callback(clone, data);
+  parentElement.appendChild(clone);
 }
 
 export async function loadTemplate(path) {
   const html = await fetch(path).then(convertToText);
-  const template = document.createElement('template');
+  const template = document.createElement("template");
   template.innerHTML = html;
   return template;
- }
+}
 
- export async function loadHeaderFooter() {
-  const header = await loadTemplate('../partials/header.html');
-  const footer = await loadTemplate('../partials/footer.html');
-  const headerElement = document.getElementById('main-header');
-  const footerElement = document.getElementById('main-footer');
+export async function loadHeaderFooter() {
+  const header = await loadTemplate("../partials/header.html");
+  const footer = await loadTemplate("../partials/footer.html");
+  const headerElement = document.getElementById("main-header");
+  const footerElement = document.getElementById("main-footer");
   renderWithTemplate(header, headerElement);
   renderWithTemplate(footer, footerElement);
- }
+}
