b4b2ae62e077014bbc786fa11b25c21ba57e8c92
DPatter52 <dzpatterson15@gmail.com>
2022-10-01 08:47:46 -0600

    small update

--- src/js/utils.js
+++ src/js/utils.js
@@ -50,7 +50,7 @@ export function renderWithTemplate(template, parentElement, data, callback) {
 }
 
 export async function loadTemplate(path) {
-  const html = await fetch(path).then(convertToText);
+  const html = await fetch(path);
   const template = document.createElement("template");
   template.innerHTML = html;
   return template;
